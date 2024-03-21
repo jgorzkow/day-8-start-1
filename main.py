83547080e6f9ba3df09a31faf66646db26be909d
jgorzkow <46491174+jgorzkow@users.noreply.github.com>
2024-03-21 03:26:01 +0000

    Function with multiple inputs

--- main.py
+++ main.py
@@ -2,11 +2,28 @@
 # Create a function called greet(). 
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
-def greet():
-  print("Hello")
-  print("How do you do?")
-  print("this is a test")
-  print("new set of code")
+# def greet():
+#   print("Hello")
+#   print("How do you do?")
+#   print("this is a test")
+#   print("new set of code")
         
-greet()
-        
+# greet()
+
+# name = input("What is your name?")
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do {name}?")
+#   print(f"this is a test {name}")
+
+
+# greet_with_name(name)
+
+#Funtion with more than 1 input
+
+location = input("Where are you from?")
+name = input("What is your name?")
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")  
+greet_with( name, location)
